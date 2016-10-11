c8619a81ec6989648e71b40822541a4625818280
Erlend Berger <erlend.berger@hotmail.com>
2016-10-11 15:29:40 -0300

    added login-screen

---
+++ app/src/main/java/com/telematica/travelmate/userinterface/login/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.telematica.travelmate.userinterface.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
