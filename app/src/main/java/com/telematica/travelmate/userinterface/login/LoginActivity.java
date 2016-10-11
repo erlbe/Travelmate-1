dfeed95eeee95ef958520030a20e7cf3524e8bcf
Erlend Berger <erlend.berger@hotmail.com>
2016-10-11 15:30:41 -0300

    added login-screen

--- app/src/main/java/com/telematica/travelmate/userinterface/login/LoginActivity.java
+++ app/src/main/java/com/telematica/travelmate/userinterface/login/LoginActivity.java
@@ -1,13 +1,37 @@
 package com.telematica.travelmate.userinterface.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.telematica.travelmate.R;
+import com.telematica.travelmate.userinterface.entrylist.EntryListActivity;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText email;
+    EditText password;
+    Button logIn;
+    Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        logIn = (Button) findViewById(R.id.login_button);
+
+        signUp = (Button) findViewById(R.id.signup_button);
+    }
+
+    /** Called when the user clicks the Log in button */
+    public void logIn(View view) {
+        // TODO: Handle login
+        Intent intent = new Intent(getApplicationContext(), EntryListActivity.class);
+        startActivity(intent);
     }
 }
