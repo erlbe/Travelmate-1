a8119da8286d4e48e30d23bc5d69fc3a0e56ced2
Erlend Berger <erlend.berger@hotmail.com>
2016-10-11 12:35:38 -0300

    set up node server and added web-views for login and signup

---
+++ travelmate-server/config/database.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Erlend on 11.10.2016.
+ */
+
+var dbuser = 'erlbe';
+var dbpassword = 'multimedios';
+
+module.exports = {
+
+    'url' : 'mongodb://'+dbuser+':'+dbpassword+'@ds011785.mlab.com:11785/travelmate' // looks like mongodb://<dbuser>:<dbpassword>@ds011785.mlab.com:11785/travelmate
+
+};
