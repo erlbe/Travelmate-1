2aeb793d6db23018e0bad0c6a4fdf4de354d0656
Erlend Berger <erlend.berger@hotmail.com>
2016-11-27 22:29:03 -0300

    added username in profile view

--- app/src/main/java/com/telematica/travelmate/userinterface/account/AccountFragment.java
+++ app/src/main/java/com/telematica/travelmate/userinterface/account/AccountFragment.java
@@ -5,21 +5,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.telematica.travelmate.R;
+import com.telematica.travelmate.model.User;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class AccountFragment extends Fragment {
 
+    TextView userNameTextView;
+
     public AccountFragment() {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_account, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        userNameTextView = (TextView) view.findViewById(R.id.usernameTextView);
+        userNameTextView.setText(User.getInstance().getName());
+        return view;
     }
 
 }
